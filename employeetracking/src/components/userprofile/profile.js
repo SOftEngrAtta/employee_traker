d92ace9b56dff63f917d0d8dbd92d34502099a62
hassi-shaikh <shaikh.aug13@gmail.com>
2018-09-20 02:09:27 +0500

    Profile Page UI developed.

--- employeetracking/src/components/userprofile/profile.js
+++ employeetracking/src/components/userprofile/profile.js
@@ -1,20 +1,75 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 
 // components 
 import Header from '../header/header';
+import './profile.css'
+export default class UserProfile extends Component {
 
-export default class UserProfile extends Component{
-    
-    updateprofile(){ console.log('profile upload functionality'); }
+    updateprofile() { console.log('profile upload functionality'); }
 
-    render(){
+    render() {
 
-        
 
-        return(
+
+        return (
             <div>
                 <Header />
-                <button onClick={ this.updateprofile.bind(this) }>Update Profile</button> 
+                <button onClick={this.updateprofile.bind(this)}>Update Profile</button>
+                <section className="">
+                    <div className="profile-main">
+                        <div className="row">
+                            <div className="col-md-3">
+                                <div className="profile-upload">
+                                    <input type="file" />
+                                    <img src="/images/user-icon.png" alt="" className="imgprofile" />
+                                </div>
+                            </div>
+                            <div className="col-md-3">
+                                <h3 className="upload-hd">Upload Picture</h3>
+                                <p className="upload-para">Max Size 1MB</p>
+                            </div>
+
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <label>Full Name</label>
+                                <input type="text" placeholder="Enter Ful Name" />
+                                <span className="hint">(e.g : john)</span>
+                            </div>
+                            <div className="col-md-6">
+                                <label>Age</label>
+                                <input type="text" placeholder="Enter Age" />
+                                <span className="hint">(e.g : 23)</span>
+                            </div>
+                            <div className="col-md-6">
+                                <label>Email Address</label>
+                                <input type="email" placeholder="Enter Email address" />
+                                <span className="hint">(e.g : abc@gmail.com)</span>
+                            </div>
+                            <div className="col-md-6">
+                                <label>Contact No</label>
+                                <input type="text" placeholder="Enter Contact No" />
+                                <span className="hint">(e.g : 111222111)</span>
+                            </div>
+                            <div className="col-md-12">
+                                <label>Address</label>
+                                <input type="text" placeholder="Enter address" />
+                                <span className="hint"></span>
+                            </div>
+                        </div>
+                        <div  className="sec-padding-xsmall bordertop sec-margin-xxsmall">
+                            <div className="row">
+                                <div className="col-md-6 ">
+                                    <a href="javascript:;" className="sec-padding-xxsmall">Click here to login.</a>
+                                </div>
+                                <div className="col-md-6 text-right">
+                                    <button className="btnmain">Submit</button> <button class="btnCancel">Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
             </div>
         )
     }
