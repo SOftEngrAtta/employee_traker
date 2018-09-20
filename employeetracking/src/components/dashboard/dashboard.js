a4a9b5cc72a9c8b8cde1e9123ca2629bd66f6a83
atta_ur_rehman <atta.rehman8123@gmail.com>
2018-09-21 03:01:38 +0500

    working on dashboard functionality

--- employeetracking/src/components/dashboard/dashboard.js
+++ employeetracking/src/components/dashboard/dashboard.js
@@ -1,11 +1,23 @@
 import React , { Component } from 'react';
 
+// css files
 import './dashboard.css';
+
+//service files 
+import { verifyuser } from '../../services/authentication.service';
+
 // components
 import Header from '../header/header';
 
 
 class Dashboard extends Component {
+
+    constructor(props){ super(props) }
+
+    componentDidMount(){
+        verifyuser();
+    }
+
     render(){
         return(
             <div className="prnt-dashboard">
--- employeetracking/src/services/authentication.service.js
+++ employeetracking/src/services/authentication.service.js
@@ -1,6 +1,13 @@
+
+// firebase import 
 import fbDB from '../config/firebasekeys'
 
+// local db storage import  
+import { getkey_data , setkey_data } from './storage.service';
+
+
 let Auth = fbDB.auth();
+
 
 /** Login Functionality
  * @param {Object} data 
@@ -15,3 +22,19 @@ export const login = (data) => Auth.signInWithEmailAndPassword(data.EmailAddress
  * @param {Object key} data.Password
  */ 
 export const createaccount = (data) => Auth.createUserWithEmailAndPassword(data.EmailAddress , data.Password);
+
+/**
+ * verfiy user by token functionality 
+ * @param { key } key // token
+ * **/ 
+
+ export const verifyuser = () => {
+    
+    let token = getkey_data({ 'KeyName' : 'auth_token'} )
+    return Auth.onIdTokenChanged((user)=>{
+        if(user && user.uid){
+            setkey_data({ 'KeyName' : 'Id' , 'KeyData' : user.uid })
+            return true 
+        }else return false
+    })
+ }
